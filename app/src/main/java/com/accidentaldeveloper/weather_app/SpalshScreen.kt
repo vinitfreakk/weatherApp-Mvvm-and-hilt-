de1b29292b8324321765dc55d0993be031062a90
vinit <vinithanabar20@gmail.com>
2023-09-30 03:16:30 +0530

    4th kick

--- app/src/main/java/com/accidentaldeveloper/weather_app/SpalshScreen.kt
+++ app/src/main/java/com/accidentaldeveloper/weather_app/SpalshScreen.kt
@@ -2,9 +2,12 @@ package com.accidentaldeveloper.weather_app
 
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.View
+import android.view.WindowManager
 import android.widget.VideoView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -14,18 +17,7 @@ class SpalshScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_spalsh_screen)
- /*       val videoView:VideoView = findViewById(R.id.video)
-        val videoPAth = "android.resource://"+ packageName + "/" + R.raw.clouds
-        val videoUri = Uri.parse(videoPAth)
-        videoView.setVideoURI(videoUri)
 
-        videoView.start()
-
-        videoView.setOnCompletionListener {
-            val intent  = Intent(this@SpalshScreen,MainActivity::class.java)
-            startActivity(intent)
-            finish()
-        }*/
 
         Handler(Looper.getMainLooper()).postDelayed({
             val intent  = Intent(this@SpalshScreen,MainActivity::class.java)
